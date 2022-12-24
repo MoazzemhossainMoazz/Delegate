7a2a3d738547978fb52c4bf69fa75bf4516fc4ab
Moazz <moazzemh222@gmail.com>
2022-12-24 23:01:14 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    public delegate void RectDel(double x, double y);
+    class Program
+    {
+        public static int AddNums(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        static void Main(string[] args)
+        {
+            Rectangle rect = new Rectangle();
+            RectDel rectDel = new RectDel(rect.GetArea);
+            rectDel += rect.GetPerimeter;
+
+            rectDel.Invoke(17.77, 40.22);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Rectangle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    class Rectangle
+    {
+        public void GetArea(double width, double height)
+        {
+            Console.WriteLine("Area of Rectangle: "+ width* height);
+        }
+
+        public void GetPerimeter(double width, double height)
+        {
+            Console.WriteLine("Perimeter of Rectangle: " + 2 * (width + height));
+        }
+
+    }
+}
